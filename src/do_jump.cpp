66dab3388be062fbe9ffbb643be79b5c79eb0d8e
christine chang <christine.chang@colorado.edu>
2023-05-05 09:49:57 -0600

    fix jump cpp

--- src/do_jump.cpp
+++ src/do_jump.cpp
@@ -22,31 +22,31 @@ int main(int argc, char** argv) {
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<pololu_maestro_ros::set_servo>("set_servo");
 
-    // // Go forward call
-    // pololu_maestro_ros::set_servo srv;
-    // srv.request.channel = 1;
-    // srv.request.target = (int) RPM;
-    // client.call(srv);
+    // Go forward call
+    pololu_maestro_ros::set_servo srv;
+    srv.request.channel = 1;
+    srv.request.target = (int) RPM;
+    client.call(srv);
 
-    // // Wait to speed up seconds
-    // usleep(FORWARD_TIME_SECONDS * MICROSECOND);
-    // // Wait for airtime
-    // usleep(AIRTIME_SECONDS * MICROSECOND);
+    // Wait to speed up seconds
+    usleep(FORWARD_TIME_SECONDS * MICROSECOND);
+    // Wait for airtime
+    usleep(AIRTIME_SECONDS * MICROSECOND);
 
-    // double RPM_DECREASE = (RPM - STOP_RPM) / SLOW_TIME_SEGMENTS;
+    double RPM_DECREASE = (RPM - STOP_RPM) / SLOW_TIME_SEGMENTS;
     
-    // for (int i =1; i <= SLOW_TIME_SEGMENTS; i++){
-    //     pololu_maestro_ros::set_servo srv;
-    //     srv.request.channel = 1;
-    //     srv.request.target = (int) (RPM - (RPM_DECREASE * i));
-    //     client.call(srv);
-    //     usleep((SLOW_TIME_SECONDS / SLOW_TIME_SEGMENTS) * MICROSECOND);
-    // }
+    for (int i =1; i <= SLOW_TIME_SEGMENTS; i++){
+        pololu_maestro_ros::set_servo srv;
+        srv.request.channel = 1;
+        srv.request.target = (int) (RPM - (RPM_DECREASE * i));
+        client.call(srv);
+        usleep((SLOW_TIME_SECONDS / SLOW_TIME_SEGMENTS) * MICROSECOND);
+    }
 
-    // // Stopping call
-    // srv.request.channel = 1;
-    // srv.request.target = STOP_RPM;
-    // client.call(srv);
+    // Stopping call
+    srv.request.channel = 1;
+    srv.request.target = STOP_RPM;
+    client.call(srv);
 
     return 0;
 }
